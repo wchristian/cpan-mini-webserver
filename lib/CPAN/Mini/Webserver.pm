232854a8fbed4d973f037fbc616ee960ea5729ce
Christian Walde <walde.christian@googlemail.com>
2011-01-12 16:43:38 +0100

    released v0.50

--- lib/CPAN/Mini/Webserver.pm
+++ lib/CPAN/Mini/Webserver.pm
@@ -49,7 +49,7 @@ has 'distvname'           => ( is => 'rw' );
 has 'filename'            => ( is => 'rw' );
 has 'index' => ( is => 'rw', isa => 'CPAN::Mini::Webserver::Index' );
 
-our $VERSION = '0.49';
+our $VERSION = '0.50';
 
 sub service_name {
     "$ENV{USER}'s minicpan_webserver";
